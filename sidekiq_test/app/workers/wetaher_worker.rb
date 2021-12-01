07f2c50b67f33a54abc61483a63fecf085fbf0c1
Dowr <dowr.atayew@mail.ru>
2021-12-01 17:00:53 +0300

    add sidekiq test

--- sidekiq_test/app/workers/wetaher_worker.rb
+++
@@ -1,7 +0,0 @@
-class WetherWorker
-  include Sidekiq::Worker
-
-  def perform
-    City.find_each { |c| WeatherMetricsService.fetch(c.id) }
-  end
-end
--- sidekiq_test/config/initializers/sidekiq.rb
+++
@@ -1,8 +0,0 @@
-Sidekiq::Extensions.enable_delay!
-Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis:localhost:6379/sidekiq_test' }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis:localhost:6379/sidekiq_test' }
-end
